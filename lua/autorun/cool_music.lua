571307b8a09699b5c9ee3f5c485b3ac5750b17e6
Jijatoro <125099175+Jijatoro@users.noreply.github.com>
2025-09-18 20:52:38 +0300

    Create work
    Load file

---
+++ lua/autorun/cool_music.lua
@@ -0,0 +1,6 @@
+if not (Guitar_Hero) then Guitar_Hero = {} Guitar_Hero.DataMusic = {} end
+Guitar_Hero.DataMusic["cool_music.mp3"] = {
+    name = "Cool music",
+    author = "Unknow",
+    complexity = 2 -- 1 bef 5
+}
